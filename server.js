cd6bf33c278578ab25015dde754a65a1fbef3f73
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-08-31 20:31:01 -0700

    get and post routes set up

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const notes = require('./db/db.json');
+
+// Express middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/assets/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/assets/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  let newNote = req.body;
+  notes.push(newNotes);
+  fs.writeFileSync('/db/db.json', JSON.stringify(notes));
+  res.json(notes);
+});
+
+// Listening
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
